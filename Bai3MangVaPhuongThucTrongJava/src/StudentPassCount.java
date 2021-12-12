4d37bf29df18a57fbb1b6a2b65adf461dee69549
NGO MAI <ngomai692@gmail.com>
2021-12-12 08:14:51 +0700

    add StudentPassCount

---
+++ Bai3MangVaPhuongThucTrongJava/src/StudentPassCount.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class StudentPassCount {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.println("Enter a size");
+            size = scanner.nextInt();
+            if (size > 30)
+            System.out.println("Size should not exceed 30");
+        }while (size > 30);
+
+        array = new int[size];
+        int i = 0;
+        while ( i < size) {
+            System.out.println("Enter a mark for student "+ (i+1)+ ":");
+            array[i] = scanner.nextInt();
+            i++;
+            }
+        int count = 0;
+        System.out.println("List of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j] + "\t");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.println("\n The number ò students pasing the exam is "+ count);
+
+    }
+
+
+
+}
