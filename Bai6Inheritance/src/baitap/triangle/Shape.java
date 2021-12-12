5cd489a02c59d18375f6dc81e2cf6152b3f9f6ce
NGO MAI <ngomai692@gmail.com>
2021-12-12 13:13:46 +0700

    add Triangle

---
+++ Bai6Inheritance/src/baitap/triangle/Shape.java
@@ -0,0 +1,4 @@
+package baitap.triangle;
+
+public class Shape {
+}
---
+++ Bai6Inheritance/src/baitap/triangle/TriangleTest.java
@@ -0,0 +1,11 @@
+package baitap.triangle;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+
+        triangle= new Triangle(1,2,4);
+        System.out.println(triangle);
+    }
+}
