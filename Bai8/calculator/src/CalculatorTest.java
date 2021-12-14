24f45076ddda72f96a843e0166f1bcae2adbbea6
NGO MAI <ngomai692@gmail.com>
2021-12-14 11:01:30 +0700

    add calcullator

--- Bai8/calculator/src/CalculatorTest.java
+++ Bai8/calculator/src/CalculatorTest.java
@@ -1,78 +1,77 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
 
-
-class CalculatorTest {
-
+public class CalculatorTest {
     @Test
     @DisplayName("Testing addition")
     void testCalculateAdd() {
-        int firstOperand = 1;
-        int secondOperand = 1;
-        char operator = '+';
+        int a = 1;
+        int b = 1;
+        char o = '+';
         int expected = 2;
 
-        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        int result = Calculator.calculate(a, b, o);
         assertEquals(expected, result);
     }
 
     @Test
     @DisplayName("Testing subtraction")
     void testCalculateSub() {
-        int firstOperand = 2;
-        int secondOperand = 1;
-        char operator = '-';
+        int a = 2;
+        int b = 1;
+        char o = '-';
         int expected = 1;
 
-        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        int result = Calculator.calculate(a, b, o);
         assertEquals(expected, result);
     }
 
     @Test
     @DisplayName("Testing multiplication")
     void testCalculateMul() {
-        int firstOperand = 2;
-        int secondOperand = 2;
-        char operator = '*';
+        int a = 2;
+        int b = 2;
+        char o = '*';
         int expected = 4;
 
-        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        int result = Calculator.calculate(a, b, o);
         assertEquals(expected, result);
     }
 
     @Test
     @DisplayName("Testing division")
     void testCalculateDiv() {
-        int firstOperand = 6;
-        int secondOperand = 3;
-        char operator = '/';
+        int a = 6;
+        int b = 3;
+        char o = '/';
         int expected = 2;
 
-        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        int result = Calculator.calculate(a, b, o);
         assertEquals(expected, result);
     }
 
     @Test
     @DisplayName("Testing division by zero")
     void testCalculateDivByZero() {
-        int firstOperand = 2;
-        int secondOperand = 0;
-        char operator = '/';
+        int a = 2;
+        int b = 0;
+        char o = '/';
 
         assertThrows(RuntimeException.class,
-                ()-> Calculator.calculate(firstOperand, secondOperand, operator));
+                ()-> {Calculator.calculate(a, b, o);});
     }
 
     @Test
     @DisplayName("Testing wrong operator")
     void testCalculateWrongOperator() {
-        int firstOperand = 2;
-        int secondOperand = 0;
-        char operator = '=';
+        int a = 2;
+        int b = 0;
+        char o = '=';
 
         assertThrows(RuntimeException.class,
-                ()-> Calculator.calculate(firstOperand, secondOperand, operator));
+                ()-> {Calculator.calculate(a, b, o);});
     }
 }
