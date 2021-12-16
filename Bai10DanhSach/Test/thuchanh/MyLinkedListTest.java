83602adb767c22bffdc86aea356c7300719b953c
NGO MAI <ngomai692@gmail.com>
2021-12-16 10:26:16 +0700

    add MyLinkedList

---
+++ Bai10DanhSach/Test/thuchanh/MyLinkedListTest.java
@@ -0,0 +1,18 @@
+package thuchanh;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= Testing /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,8);
+        ll.printList();
+
+    }
+}
