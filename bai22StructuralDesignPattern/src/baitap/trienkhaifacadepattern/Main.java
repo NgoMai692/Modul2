7960c1759bf222f88476acb634bf87ff9254c874
NGO MAI <ngomai692@gmail.com>
2022-01-04 17:32:49 +0700

    add trienkhaifacadepattern

---
+++ bai22StructuralDesignPattern/src/baitap/trienkhaifacadepattern/Main.java
@@ -0,0 +1,7 @@
+package baitap.trienkhaifacadepattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ bai22StructuralDesignPattern/src/baitap/trienkhaifacadepattern/RandomListFacade.java
@@ -0,0 +1,12 @@
+package baitap.trienkhaifacadepattern;
+
+public class RandomListFacade {
+    private ListFilter listFilter;
+    private ListPrinter listPrinter;
+    private RandomList randomList;
+
+    public void printRandomEvenList(){
+
+    }
+
+}
